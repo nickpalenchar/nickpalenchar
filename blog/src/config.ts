784e14f27af2564344e98b47be2671a08683193e
Nick Palenchar <nickpal@nickpalenchar.com>
2024-12-31 11:18:15 -0500

    update config

--- blog/src/config.ts
+++ blog/src/config.ts
@@ -1,11 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "Nick Palenchar";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "";
+export const TWITTER_HANDLE = "";
+export const MY_NAME = "Nick Palenchar";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
